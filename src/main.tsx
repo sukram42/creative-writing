d75d76c9478c918621014510c4235a32741e1ad1
Markus Böbel <markus.boebel@tum.de>
2024-04-21 00:15:28 +0200

    fix ci

--- src/main.tsx
+++ src/main.tsx
@@ -11,10 +11,10 @@ import { store } from './app/store.ts';
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path="/" element={<RequireAuth><Layout /></RequireAuth>}>
+      <Route path="/creative-writing" element={<RequireAuth><Layout /></RequireAuth>}>
         <Route index element={<MainView />} />
       </Route>
-      <Route path="/login" element={<LoginView />} />
+      <Route path="/creative-writing/login" element={<LoginView />} />
     </>
   )
 );
